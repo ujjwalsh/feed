ujjwalsh/feed

commit f077406b845df215f1550b0702d95bc3264deabe

Switch core types to Text and xml-types
Do not fix type errors yet except the most basic stuff

src/Text/Atom/Feed/Export.hs
@@ -55,90 +55,89 @@ module Text.Atom.Feed.Export
   , mb
   ) where
 
-import Text.XML.Light as XML
+import Data.Text (Text, pack)
+import Data.XML.Types as XML
 import Text.Atom.Feed
 
-atom_prefix :: Maybe String
+atom_prefix :: Maybe Text
 atom_prefix = Nothing -- Just "atom"
 
-atom_thr_prefix :: Maybe String
+atom_thr_prefix :: Maybe Text
 atom_thr_prefix = Just "thr"
 
-atomNS :: String
+atomNS :: Text
 atomNS = "http://www.w3.org/2005/Atom"
 
-atomThreadNS :: String
+atomThreadNS :: Text
 atomThreadNS = "http://purl.org/syndication/thread/1.0"
 
+blank_element :: Name -> [Node] -> XML.Element
+blank_element name content = XML.Element name [] content
+
 xmlns_atom :: Attr
-xmlns_atom = Attr qn atomNS
+xmlns_atom = (qn, [ContentText atomNS])
  where
   qn = case atom_prefix of
-         Nothing -> QName { qName   = "xmlns"
-                          , qURI    = Nothing
-                          , qPrefix = Nothing
-                          }
-         Just s  -> QName { qName   = s
-                          , qURI    = Nothing   -- XXX: is this ok?
-                          , qPrefix = Just "xmlns"
-                          }
+         Nothing -> Name { nameLocalName   = "xmlns"
+                         , nameNamespace    = Nothing
+                         , namePrefix = Nothing
+                         }
+         Just s  -> Name { nameLocalName   = s
+                         , nameNamespace    = Nothing   -- XXX: is this ok?
+                         , namePrefix = Just "xmlns"
+                         }
 
 xmlns_atom_thread :: Attr
-xmlns_atom_thread = Attr qn atomThreadNS
+xmlns_atom_thread = (qn, [ContentText atomThreadNS])
  where
   qn = case atom_prefix of
-         Nothing -> QName { qName   = "xmlns"
-                          , qURI    = Nothing
-                          , qPrefix = Nothing
-                          }
-         Just s  -> QName { qName   = s
-                          , qURI    = Nothing   -- XXX: is this ok?
-                          , qPrefix = Just "xmlns"
-                          }
+         Nothing -> Name { nameLocalName   = "xmlns"
+                         , nameNamespace    = Nothing
+                         , namePrefix = Nothing
+                         }
+         Just s  -> Name { nameLocalName   = s
+                         , nameNamespace    = Nothing   -- XXX: is this ok?
+                         , namePrefix = Just "xmlns"
+                         }
 
-atomName :: String -> QName
-atomName nc   = QName { qName   = nc
-                      , qURI    = Just atomNS
-                      , qPrefix = atom_prefix
-                      }
+atomName :: Text -> Name
+atomName nc = Name { nameLocalName = nc
+                   , nameNamespace = Just atomNS
+                   , namePrefix    = atom_prefix
+                   }
 
-atomAttr :: String -> String -> Attr
-atomAttr x y  = Attr (atomName x) y
+atomAttr :: Text -> Text -> Attr
+atomAttr x y = (atomName x, [ContentText y])
 
-atomNode :: String -> [XML.Content] -> XML.Element
-atomNode x xs = blank_element { elName = atomName x, elContent = xs }
+atomNode :: Text -> [Node] -> XML.Element
+atomNode x xs = blank_element (atomName x) xs
 
-atomLeaf :: String -> String -> XML.Element
-atomLeaf tag txt = blank_element
-                     { elName    = atomName tag
-                     , elContent = [ Text blank_cdata { cdData = txt } ]
-                     }
+atomLeaf :: Text -> Text -> XML.Element
+atomLeaf tag txt =
+  blank_element (atomName tag) [NodeContent $ ContentText txt]
 
-atomThreadName :: String -> QName
-atomThreadName nc =
-  QName { qName   = nc
-        , qURI    = Just atomThreadNS
-        , qPrefix = atom_thr_prefix
-        }
+atomThreadName :: Text -> Name
+atomThreadName nc = Name { nameLocalName = nc
+                         , nameNamespace = Just atomThreadNS
+                         , namePrefix    = atom_thr_prefix
+                         }
 
-atomThreadAttr :: String -> String -> Attr
-atomThreadAttr x y  = Attr (atomThreadName x) y
+atomThreadAttr :: Text -> Text -> Attr
+atomThreadAttr x y = (atomThreadName x, [ContentText y])
 
-atomThreadNode :: String -> [XML.Content] -> XML.Element
+atomThreadNode :: Text -> [Node] -> XML.Element
 atomThreadNode x xs =
-  blank_element { elName = atomThreadName x, elContent = xs }
+  blank_element (atomThreadName x) xs
 
-atomThreadLeaf :: String -> String -> XML.Element
+atomThreadLeaf :: Text -> Text -> XML.Element
 atomThreadLeaf tag txt =
-  blank_element { elName = atomThreadName tag
-                , elContent = [ Text blank_cdata { cdData = txt } ]
-                }
+  blank_element (atomThreadName tag) [NodeContent $ ContentText txt]
 
 --------------------------------------------------------------------------------
 
 xmlFeed :: Feed -> XML.Element
 xmlFeed f = ( atomNode "feed"
-          $ map Elem
+          $ map NodeElement
           $ [ xmlTitle (feedTitle f) ]
          ++ [ xmlId (feedId f) ]
          ++ [ xmlUpdated (feedUpdated f) ]
@@ -154,12 +153,12 @@ xmlFeed f = ( atomNode "feed"
          ++ map xmlEntry (feedEntries f)
          ++ feedOther f )
 
-            { elAttribs = [xmlns_atom] }
+            { elementAttributes = [xmlns_atom] }
 
 
 xmlEntry :: Entry -> XML.Element
 xmlEntry e  = ( atomNode "entry"
-            $ map Elem
+            $ map NodeElement
             $ [ xmlId (entryId e) ]
            ++ [ xmlTitle (entryTitle e) ]
            ++ [ xmlUpdated (entryUpdated e) ]
@@ -176,38 +175,38 @@ xmlEntry e  = ( atomNode "entry"
            ++ mb  xmlInReplyTotal (entryInReplyTotal e)
            ++ entryOther e )
 
-              { elAttribs = entryAttrs e }
+              { elementAttributes = entryAttrs e }
 
 xmlContent :: EntryContent -> XML.Element
 xmlContent cont = case cont of
 
   TextContent t -> (atomLeaf "content" t)
-                      { elAttribs = [ atomAttr "type" "text" ] }
+                      { elementAttributes = [ atomAttr "type" "text" ] }
 
   HTMLContent t -> (atomLeaf "content" t)
-                      { elAttribs = [ atomAttr "type" "html" ] }
+                      { elementAttributes = [ atomAttr "type" "html" ] }
 
-  XHTMLContent x -> (atomNode "content" [ Elem x ])
-                      { elAttribs = [ atomAttr "type" "xhtml" ] }
+  XHTMLContent x -> (atomNode "content" [ NodeElement x ])
+                      { elementAttributes = [ atomAttr "type" "xhtml" ] }
 
   MixedContent mbTy cs -> (atomNode "content" cs)
-                             { elAttribs = mb (atomAttr "type") mbTy }
+                             { elementAttributes = mb (atomAttr "type") mbTy }
 
   ExternalContent mbTy src -> (atomNode "content" [])
-                                 { elAttribs = [ atomAttr "src" src ]
+                                 { elementAttributes = [ atomAttr "src" src ]
                                             ++ mb (atomAttr "type") mbTy }
 
 
 xmlCategory :: Category -> XML.Element
-xmlCategory c = (atomNode "category" (map Elem (catOther c)))
-                  { elAttribs = [ atomAttr "term" (catTerm c) ]
+xmlCategory c = (atomNode "category" (map NodeElement (catOther c)))
+                  { elementAttributes = [ atomAttr "term" (catTerm c) ]
                                ++ mb (atomAttr "scheme") (catScheme c)
                                ++ mb (atomAttr "label") (catLabel c)
                   }
 
 xmlLink :: Link -> XML.Element
-xmlLink l = (atomNode "link" (map Elem (linkOther l)))
-              { elAttribs = [ atomAttr "href" (linkHref l) ]
+xmlLink l = (atomNode "link" (map NodeElement (linkOther l)))
+              { elementAttributes = [ atomAttr "href" (linkHref l) ]
                         ++ mb (atomAttr "rel" . either id id) (linkRel l)
                         ++ mb (atomAttr "type") (linkType l)
                         ++ mb (atomAttr "hreflang") (linkHrefLang l)
@@ -218,7 +217,7 @@ xmlLink l = (atomNode "link" (map Elem (linkOther l)))
 
 xmlSource :: Source -> Element
 xmlSource s = atomNode "source"
-            $ map Elem
+            $ map NodeElement
             $ sourceOther s
            ++ map xmlAuthor (sourceAuthors s)
            ++ map xmlCategory (sourceCategories s)
@@ -235,7 +234,7 @@ xmlSource s = atomNode "source"
 
 xmlGenerator :: Generator -> Element
 xmlGenerator g = (atomLeaf "generator" (genText g))
-                    { elAttribs = mb (atomAttr "uri") (genURI g)
+                    { elementAttributes = mb (atomAttr "uri") (genURI g)
                                ++ mb (atomAttr "version") (genVersion g)
                     }
 
@@ -246,8 +245,8 @@ xmlAuthor p = atomNode "author" (xmlPerson p)
 xmlContributor :: Person -> XML.Element
 xmlContributor c = atomNode "contributor" (xmlPerson c)
 
-xmlPerson :: Person -> [XML.Content]
-xmlPerson p = map Elem $
+xmlPerson :: Person -> [XML.Node]
+xmlPerson p = map NodeElement $
             [ atomLeaf "name" (personName p) ]
            ++ mb (atomLeaf "uri")   (personURI p)
            ++ mb (atomLeaf "email") (personEmail p)
@@ -256,7 +255,7 @@ xmlPerson p = map Elem $
 xmlInReplyTo :: InReplyTo -> XML.Element
 xmlInReplyTo irt =
      (atomThreadNode "in-reply-to" (replyToContent irt))
-                 { elAttribs =
+                 { elementAttributes =
                        mb (atomThreadAttr "ref")  (Just $ replyToRef irt)
                     ++ mb (atomThreadAttr "href") (replyToHRef irt)
                     ++ mb (atomThreadAttr "type") (replyToType irt)
@@ -266,10 +265,10 @@ xmlInReplyTo irt =
 
 xmlInReplyTotal :: InReplyTotal -> XML.Element
 xmlInReplyTotal irt =
-     (atomThreadLeaf "total" (show $ replyToTotal irt))
-                 { elAttribs = replyToTotalOther irt }
+     (atomThreadLeaf "total" (pack $ show $ replyToTotal irt))
+                 { elementAttributes = replyToTotalOther irt }
 
-xmlId :: String -> XML.Element
+xmlId :: Text -> XML.Element
 xmlId i = atomLeaf "id" i
 
 xmlIcon :: URI -> XML.Element
@@ -296,13 +295,13 @@ xmlSubtitle s = xmlTextContent "subtitle" s
 xmlSummary :: TextContent -> XML.Element
 xmlSummary s = xmlTextContent "summary" s
 
-xmlTextContent :: String -> TextContent -> XML.Element
+xmlTextContent :: Text -> TextContent -> XML.Element
 xmlTextContent tg t =
   case t of
-    TextString s  -> (atomLeaf tg s) { elAttribs = [atomAttr "type" "text"] }
-    HTMLString s  -> (atomLeaf tg s) { elAttribs = [atomAttr "type" "html"] }
-    XHTMLString e -> (atomNode tg [XML.Elem e])
-                          { elAttribs = [atomAttr "type" "xhtml"] }
+    TextString s  -> (atomLeaf tg s) { elementAttributes = [atomAttr "type" "text"] }
+    HTMLString s  -> (atomLeaf tg s) { elementAttributes = [atomAttr "type" "html"] }
+    XHTMLString e -> (atomNode tg [XML.NodeElement e])
+                          { elementAttributes = [atomAttr "type" "xhtml"] }
 
 --------------------------------------------------------------------------------
 mb :: (a -> b) -> Maybe a -> [b]

src/Text/Atom/Pub/Export.hs
@@ -14,8 +14,7 @@
 --
 --------------------------------------------------------------------
 module Text.Atom.Pub.Export
-  ( showServiceDoc
-  , mkQName
+  ( mkQName
   , mkElem
   , mkLeaf
   , mkAttr
@@ -28,38 +27,38 @@ module Text.Atom.Pub.Export
   , xmlAccept
   ) where
 
-import Text.XML.Light
+import Data.Text (Text)
+import Data.XML.Types
 import Text.Atom.Pub
 import Text.Atom.Feed.Export
        ( mb, xmlCategory, xmlTitle
        , xmlns_atom
        )
 
-showServiceDoc :: Service -> String
-showServiceDoc s = showElement (xmlService s)
+type Attr = (Name, [Content])
 
 -- ToDo: old crud; inline away.
-mkQName :: Maybe String -> String -> QName
-mkQName a b = blank_name{qPrefix=a,qName=b}
+mkQName :: Maybe Text -> Text -> Name
+mkQName a b = Name b a Nothing
 
-mkElem :: QName -> [Attr] -> [Element] -> Element
-mkElem a b c = node a ((b::[Attr]),(c::[Element]))
+mkElem :: Name -> [Attr] -> [Element] -> Element
+mkElem a b c = Element a b $ map NodeElement c
 
-mkLeaf :: QName -> [Attr] -> String -> Element
-mkLeaf a b c = node (a::QName) ((b::[Attr]),[Text blank_cdata{cdData=c}])
+mkLeaf :: Name -> [Attr] -> Text -> Element
+mkLeaf a b c = Element a b [NodeContent $ ContentText c]
 
-mkAttr :: String -> String -> Attr
-mkAttr a b  = Attr blank_name{qName=a} b
+mkAttr :: Text -> Text -> Attr
+mkAttr a b = (Name a Nothing Nothing, [ContentText b])
 
 xmlns_app :: Attr
-xmlns_app = Attr (mkQName (Just "xmlns") "app") appNS
+xmlns_app = (mkQName (Just "xmlns") "app", [ContentText appNS])
 
 
-appNS :: String
+appNS :: Text
 appNS = "http://purl.org/atom/app#"
 
-appName :: String -> QName
-appName nc = (mkQName (Just "app") nc){qURI=Just appNS}
+appName :: Text -> Name
+appName nc = (mkQName (Just "app") nc){nameNamespace = Just appNS}
 
 xmlService :: Service -> Element
 xmlService s =

src/Text/Feed/Export.hs
@@ -23,7 +23,7 @@ import Text.Atom.Feed.Export as Atom
 import Text.RSS.Export as RSS
 import Text.RSS1.Export as RSS1
 
-import Text.XML.Light as XML
+import Data.XML.Types as XML
 
 -- | 'xmlFeed f' serializes a @Feed@ document into a conforming
 -- XML toplevel element.

src/Text/RSS/Export.hs
@@ -36,6 +36,7 @@ import Text.XML.Light as XML
 import Text.RSS.Syntax
 
 import Data.Maybe
+import Data.Text (pack)
 
 qualNode :: String -> [XML.Content] -> XML.Element
 qualNode n cs =
@@ -73,7 +74,7 @@ xmlChannel ch =
       ++ mb (xmlLeaf "generator") (rssGenerator ch)
       ++ mb (xmlLeaf "docs") (rssDocs ch)
       ++ mb xmlCloud (rssCloud ch)
-      ++ mb ((xmlLeaf "ttl") . show) (rssTTL ch)
+      ++ mb ((xmlLeaf "ttl") . pack . show) (rssTTL ch)
       ++ mb xmlImage (rssImage ch)
       ++ mb (xmlLeaf "rating") (rssRating ch)
       ++ mb xmlTextInput (rssTextInput ch)
@@ -138,8 +139,8 @@ xmlImage im =
        , xmlLeaf "title" (rssImageTitle im)
        , xmlLeaf "link"  (rssImageLink im)
        ]
-       ++ mb ((xmlLeaf "width")  . show) (rssImageWidth im)
-       ++ mb ((xmlLeaf "height") . show) (rssImageHeight im)
+       ++ mb ((xmlLeaf "width")  . pack . show) (rssImageWidth im)
+       ++ mb ((xmlLeaf "height") . pack . show) (rssImageHeight im)
        ++ mb (xmlLeaf "description") (rssImageDesc im)
        ++ rssImageOther im))
 
@@ -167,7 +168,7 @@ xmlTextInput ti =
 xmlSkipHours :: [Integer] -> XML.Element
 xmlSkipHours hs =
   (qualNode "skipHours" $ map Elem $
-    (map (\ n -> xmlLeaf "hour" (show n)) hs))
+    (map (\ n -> xmlLeaf "hour" (pack $ show n)) hs))
 
 xmlSkipDays :: [String] -> XML.Element
 xmlSkipDays hs =

feed.cabal
@@ -70,10 +70,12 @@ library
       base >= 4 && < 4.11
     , old-locale == 1.0.*
     , old-time >= 1 && < 1.2
+    , text < 1.3
     , time < 1.9
     , time-locale-compat == 0.1.*
     , utf8-string < 1.1
-    , xml >= 1.2.6 && < 1.3.15
+    , xml-types >= 0.3.6 && < 0.4
+    , xml-conduit >= 1.3 && < 1.6
 
 test-suite tests
   ghc-options:       -Wall

src/Text/Atom/Feed.hs
@@ -16,6 +16,7 @@ module Text.Atom.Feed
   , NCName
   , Date
   , MediaType
+  , Attr
   , Feed (..)
   , Entry (..)
   , EntryContent (..)
@@ -37,20 +38,23 @@ module Text.Atom.Feed
   , nullPerson
   ) where
 
-import qualified Text.XML.Light as XML
+import Data.Text (Text, unpack)
+import Data.XML.Types as XML
 
 -- *Core types
 
 -- NOTE: In the future we may want to have more structured
 -- types for these.
-type URI        = String
-type NCName     = String
-type Date       = String
-type MediaType  = String
+type URI        = Text
+type NCName     = Text
+type Date       = Text
+type MediaType  = Text
+
+type Attr = (Name, [Content])
 
 data Feed
  = Feed
-      { feedId           :: String
+      { feedId           :: URI
       , feedTitle        :: TextContent
       , feedUpdated      :: Date
       , feedAuthors      :: [Person]
@@ -63,14 +67,14 @@ data Feed
       , feedRights       :: Maybe TextContent
       , feedSubtitle     :: Maybe TextContent
       , feedEntries      :: [Entry]
-      , feedAttrs        :: [XML.Attr]
+      , feedAttrs        :: [Attr]
       , feedOther        :: [XML.Element]
       }
      deriving (Show)
 
 data Entry
  = Entry
-      { entryId           :: String
+      { entryId           :: URI
       , entryTitle        :: TextContent
       , entryUpdated      :: Date
       , entryAuthors      :: [Person]
@@ -84,24 +88,24 @@ data Entry
       , entrySummary      :: Maybe TextContent
       , entryInReplyTo    :: Maybe InReplyTo
       , entryInReplyTotal :: Maybe InReplyTotal
-      , entryAttrs        :: [XML.Attr]
+      , entryAttrs        :: [Attr]
       , entryOther        :: [XML.Element]
       }
      deriving (Show)
 
 data EntryContent
- = TextContent   String
- | HTMLContent   String
+ = TextContent   Text
+ | HTMLContent   Text
  | XHTMLContent  XML.Element
- | MixedContent  (Maybe String) [XML.Content]
+ | MixedContent  (Maybe Text) [XML.Node]
  | ExternalContent (Maybe MediaType) URI
      deriving (Show)
 
 data Category
  = Category
-       { catTerm   :: String         -- ^ the tag\/term of the category.
+       { catTerm   :: Text         -- ^ the tag\/term of the category.
        , catScheme :: Maybe URI      -- ^ optional URL for identifying the categorization scheme.
-       , catLabel  :: Maybe String   -- ^ human-readable label of the category
+       , catLabel  :: Maybe Text   -- ^ human-readable label of the category
        , catOther  :: [XML.Element]  -- ^ unknown elements, for extensibility.
        }
      deriving (Show)
@@ -110,8 +114,8 @@ data Category
 data Generator
  = Generator
        { genURI     :: Maybe URI
-       , genVersion :: Maybe String
-       , genText    :: String
+       , genVersion :: Maybe Text
+       , genText    :: Text
        }
      deriving (Eq, Show)
 
@@ -121,23 +125,23 @@ data Link
          -- ToDo: make the switch over to using the Atom.Feed.Link relation type.
       , linkRel      :: Maybe (Either NCName URI)
       , linkType     :: Maybe MediaType
-      , linkHrefLang :: Maybe String
-      , linkTitle    :: Maybe String
-      , linkLength   :: Maybe String
-      , linkAttrs    :: [XML.Attr]
+      , linkHrefLang :: Maybe Text
+      , linkTitle    :: Maybe Text
+      , linkLength   :: Maybe Text
+      , linkAttrs    :: [Attr]
       , linkOther    :: [XML.Element]
       }
      deriving (Show)
 
 data TextContent
- = TextString  String
- | HTMLString  String
+ = TextString  Text
+ | HTMLString  Text
  | XHTMLString XML.Element
      deriving (Show)
 
 txtToString :: TextContent -> String
-txtToString (TextString s) = s
-txtToString (HTMLString s) = s
+txtToString (TextString s) = unpack s
+txtToString (HTMLString s) = unpack s
 txtToString (XHTMLString x) = show x
 
 data Source
@@ -146,7 +150,7 @@ data Source
       , sourceCategories  :: [Category]
       , sourceGenerator   :: Maybe Generator
       , sourceIcon        :: Maybe URI
-      , sourceId          :: Maybe String
+      , sourceId          :: Maybe URI
       , sourceLinks       :: [Link]
       , sourceLogo        :: Maybe URI
       , sourceRights      :: Maybe TextContent
@@ -160,9 +164,9 @@ data Source
 
 data Person
  = Person
-     { personName  :: String
+     { personName  :: Text
      , personURI   :: Maybe URI
-     , personEmail :: Maybe String
+     , personEmail :: Maybe Text
      , personOther :: [XML.Element]
      }
      deriving (Show)
@@ -173,21 +177,21 @@ data InReplyTo
      , replyToHRef    :: Maybe URI
      , replyToType    :: Maybe MediaType
      , replyToSource  :: Maybe URI
-     , replyToOther   :: [XML.Attr]
-     , replyToContent :: [XML.Content]
+     , replyToOther   :: [Attr]
+     , replyToContent :: [Node]
      }
      deriving (Show)
 
 data InReplyTotal
  = InReplyTotal
      { replyToTotal      :: Integer -- non-negative :)
-     , replyToTotalOther :: [XML.Attr]
+     , replyToTotalOther :: [Attr]
      }
      deriving (Show)
 
 -- *Smart Constructors
 
-newCategory :: String -- ^catTerm
+newCategory :: Text -- ^catTerm
             -> Category
 newCategory t = Category
   { catTerm   = t
@@ -196,7 +200,7 @@ newCategory t = Category
   , catOther  = []
   }
 
-nullFeed :: String  -- ^feedId
+nullFeed :: URI  -- ^feedId
          -> TextContent -- ^feedTitle
          -> Date -- ^feedUpdated
          -> Feed
@@ -218,7 +222,7 @@ nullFeed i t u = Feed
       , feedOther        = []
       }
 
-nullEntry :: String -- ^entryId
+nullEntry :: URI -- ^entryId
           -> TextContent -- ^entryTitle
           -> Date -- ^entryUpdated
           -> Entry
@@ -242,7 +246,7 @@ nullEntry i t u = Entry
       }
 
 
-nullGenerator :: String -- ^genText
+nullGenerator :: Text -- ^genText
               -> Generator
 nullGenerator t = Generator
   { genURI     = Nothing

commit 7bd18d381cfe6d099427455c9d9ef5a15d70374d

Get rid of xmlns:ns and ns:-prefixed attributes
It seems that `xml-conduit` treats attributes with namespaces
differently than `xml` did and produces the following output:

```xml
<content xmlns:ns="http://www.w3.org/2005/Atom" ns:type="html">
```
instead of

```xml
<content type="html">
```

which seems to confuse some feed readers, notably
<https://github.com/lwindolf/liferea>.

If I'm interpreting https://stackoverflow.com/a/46865/3407728 correctly,
it seems that these attributes indeed aren't meant to be namespaced.
Indeed, if I inspect some `<a href="whatever">` element in a browser
using the javascript console, the element does have a namespace but the
attribute does not.

Therefore, drop the namespace from attributes.

(This is a regression introduced in f077406b845df215f. Older versions of
the library generated correct XML. Also, while fixing the examples in
README, a forgotten change of content type from 76e9f5bead56e40e8 is
applied.)

src/Text/Atom/Feed/Export.hs
@@ -107,7 +107,7 @@ atomName :: Text -> Name
 atomName nc = Name {nameLocalName = nc, nameNamespace = Just atomNS, namePrefix = atom_prefix}
 
 atomAttr :: Text -> Text -> Attr
-atomAttr x y = (atomName x, [ContentText y])
+atomAttr x y = (Name {nameLocalName = x, nameNamespace = Nothing, namePrefix = atom_prefix}, [ContentText y])
 
 atomNode :: Text -> [Node] -> XML.Element
 atomNode x = blank_element (atomName x)