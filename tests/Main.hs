7f30d891a7bb2729b76edbdd9ec3726a24826b59
Tomas Janousek <tomi@nomi.cz>
2019-10-27 21:48:28 +0100

    Test that export ∘ import = id (just atom, for now)
    This is meant to ensure that a standards-compliant atom feed can be
    imported without losing information (see 143c161e2d) and exported
    without introducing weird reader-confusing glitches (see 7bd18d381c).

--- tests/Main.hs
+++ tests/Main.hs
@@ -5,10 +5,11 @@ module Main
 import Prelude.Compat
 
 import Example (exampleTests)
+import ImportExport (importExportTests)
 import Test.Framework (defaultMain)
 import Text.Atom.Tests (atomTests)
 import Text.Feed.Util.Tests (feedUtilTests)
 import Text.RSS.Tests (rssTests)
 
 main :: IO ()
-main = defaultMain [rssTests, atomTests, feedUtilTests, exampleTests]
+main = defaultMain [rssTests, atomTests, feedUtilTests, exampleTests, importExportTests]
