b459f4d0ee5b3f3f9239127b11a8c7345878871b
Dmitry Dzhus <dima@dzhus.org>
2020-05-25 12:55:42 +0100

    Try adding -package manually to fix build on GHC 7.x

--- tests/doctest-driver.hs
+++ tests/doctest-driver.hs
@@ -1,1 +1,1 @@
-{-# OPTIONS_GHC -F -pgmF doctest-driver-gen -optF README.lhs -optF -pgmL -optF markdown-unlit -optF -XOverloadedStrings -optF -XNoImplicitPrelude #-}
+{-# OPTIONS_GHC -F -pgmF doctest-driver-gen -optF README.lhs -optF -pgmL -optF markdown-unlit -optF -XOverloadedStrings -optF -XNoImplicitPrelude -optF -package -optF text -optF -package -optF xml-types #-}
